89de3d963415aa7586bf8f5597c71645e3571069
Francesco Improta <francesco@designabile.com>
2015-07-31 17:15:16 +0200

    Added mobile nav children menu

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,13 +16,25 @@ $(document).ready(function(){
     $('.header').toggleClass('is-nav-sliding');
   });
 
+  //mobile nav - open/close navigation
   $('.mobile-nav-trigger').on('click', function(event){
     event.preventDefault();    
     $('.header').toggleClass('is-nav-open');
     $("body").toggleClass("is-locked");
   });
 
-    $('.close-button').on('click', function(event){
+  $('.has-children').children('a').on('click', function(event){
+    event.preventDefault();
+    $(this).next('.nav-children').removeClass('is-hidden');
+  });
+
+  $('.go-back').on('click', function(event){
+    event.preventDefault();    
+    $('.nav-children').addClass('is-hidden');
+  });
+
+  //mobile nav close button
+  $('.close-button').on('click', function(event){
     event.preventDefault();    
     $(".header").removeClass("is-nav-open");
     $("body").removeClass("is-locked");
